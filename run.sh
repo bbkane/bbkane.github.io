44e476eb28d60887569ec0d912c8e6c8bce86a8b
Benjamin Kane <bbk1524@gmail.com>
2017-01-24 03:09:38 +0000

    stuff

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # On my Linux VM, Firefox isn't installed.
 if [[ "$(uname)" == "Darwin" ]]; then
-    (sleep 5 && open 'http://127.0.0.1:4000/') &
+    (echo "Will open the site in 5 sec" && sleep 5 && open 'http://127.0.0.1:4000/') &
 fi
 
-bundle exec jekyll serve --host 0.0.0.0 --watch --drafts
+bundle exec jekyll serve --host 0.0.0.0 --watch --drafts --incremental
