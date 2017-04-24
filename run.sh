d60073da2ccd5936a3e58cf28b4f7d983e51872d
Benjamin Kane <bbk1524@gmail.com>
2017-04-25 02:39:52 +0500

    move some drafts to posts

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ if [[ "$(uname)" == "Darwin" ]]; then
     (echo "Will open the site in 5 sec" && sleep 5 && open 'http://127.0.0.1:4000/') &
 fi
 
-bundle exec jekyll serve --host 0.0.0.0 --watch --drafts --incremental
+# If it's taking too long, use `--incremental`, but I've noticed
+# that it doesn't rebuild the index page so I'm taking it out for now
+bundle exec jekyll serve --host 0.0.0.0 --watch --drafts
